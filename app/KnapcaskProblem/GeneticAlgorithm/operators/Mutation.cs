87a28195d6515bdbefa7b4743025a5f3419dedfb
eshulankina <eljogar@mail.ru>
2018-04-14 14:07:22 +0300

    Updated mutation

--- app/KnapcaskProblem/GeneticAlgorithm/operators/Mutation.cs
+++ app/KnapcaskProblem/GeneticAlgorithm/operators/Mutation.cs
@@ -23,11 +23,11 @@ namespace Algorithm
                 var k = m_random.Next(individ.SIZE);
                 if (m_random.Next(100) == 1) individ.GENOTYPE[k] = individs[i].GENOTYPE[k] == 0 ? 1 : 0;
                 var zeroCount = 0;
-                Array.ForEach(individs[i].GENOTYPE, gen =>
+                Array.ForEach(individ.GENOTYPE, gen =>
                 {
                     if (gen == 0) ++zeroCount;
                 });
-                if (zeroCount != individs[i].SIZE) population.Add(individ);
+                if (zeroCount != individ.SIZE) population.Add(individ);
             }
             for (var i = s; i < individs.Count; ++i) population.Add(individs[i]);
             return population;
@@ -56,7 +56,7 @@ namespace Algorithm
                     }
                 }
                 var zeroCount = 0;
-                Array.ForEach(individs[i].GENOTYPE, gen =>
+                Array.ForEach(individ.GENOTYPE, gen =>
                 {
                     if (gen == 0) ++zeroCount;
                 });
@@ -72,31 +72,33 @@ namespace Algorithm
         public List<Individ> Run(List<Individ> individs)
         {
             Logger.Get().Debug("Called " + Convert.ToString(this));
-            List<Individ> population = new List<Individ>();
-            int k = 0;
-            int r = 0;
-            int s = (int)(individs.Count - Math.Sqrt(individs.Count));
-            for (int i = 0; i < s; i++)
+            var population = new List<Individ>();
+            var s = (int)(individs.Count - Math.Sqrt(individs.Count));
+            for (var i = 0; i < s; ++i)
             {
-                Individ individ = individs[i];
-                k = m_random.Next(individs[i].SIZE - 2);
-                r = m_random.Next(k + 1, individs[i].SIZE - 1);
+                var individ = individs[i];
+                var k = m_random.Next(individ.SIZE - 2);
+                var r = m_random.Next(k + 1, individ.SIZE - 1);
 
                 if (m_random.Next(100) == 1)
                 {
-                    for (int j = 0; j <= k; j++)
+                    for (var j = 0; j <= k; ++j)
+                    {
                         individ.GENOTYPE[j] = individs[i].GENOTYPE[j] == 0 ? 1 : 0;
-                    for (int j = r; j < individs[i].SIZE; j++)
+                    }
+                    for (var j = r; j < individs[i].SIZE; ++j)
+                    {
                         individ.GENOTYPE[j] = individs[i].GENOTYPE[j] == 0 ? 1 : 0;
+                    }
                 }
                 var zeroCount = 0;
-                Array.ForEach(individs[i].GENOTYPE, gen =>
+                Array.ForEach(individ.GENOTYPE, gen =>
                 {
                     if (gen == 0) ++zeroCount;
                 });
-                if (zeroCount != individs[i].SIZE) population.Add(individ);
+                if (zeroCount != individ.SIZE) population.Add(individ);
             }
-            for (int i = s; i < individs.Count; i++) population.Add(individs[i]);
+            for (var i = s; i < individs.Count; ++i) population.Add(individs[i]);
             return population;
         }
     }
@@ -108,8 +110,8 @@ namespace Algorithm
         {
             Logger.Get().Debug("Called " + Convert.ToString(this));
             var population = new List<Individ>();
-            int s = (int)(individs.Count - Math.Sqrt(individs.Count));
-            for (int i = 0; i < s; i++)
+            var s = (int)(individs.Count - Math.Sqrt(individs.Count));
+            for (var i = 0; i < s; ++i)
             {
                 var individ = individs[i];
 
@@ -122,7 +124,7 @@ namespace Algorithm
                     individ.GENOTYPE[k] = individs[i].GENOTYPE[r];
                 }
                 var zeroCount = 0;
-                Array.ForEach(individs[i].GENOTYPE, gen =>
+                Array.ForEach(individ.GENOTYPE, gen =>
                 {
                     if (gen == 0) ++zeroCount;
                 });
