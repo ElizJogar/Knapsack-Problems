ebb5caf3f8b648fd129ec3ea4fdc7ab559ceed16
eshulankina <eljogar@mail.ru>
2018-04-16 12:06:12 +0300

    Added constraint processing

--- app/KnapcaskProblem/GeneticAlgorithm/operators/ConstraintProcessing.cs
+++ app/KnapcaskProblem/GeneticAlgorithm/operators/ConstraintProcessing.cs
@@ -1,1 +1,70 @@
-﻿k
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KnapsackProblem;
+using CustomLogger;
+
+namespace Algorithm
+{
+    public interface IConstraintProcessing : IOperator
+    {
+        List<IndividEx> Run(List<Individ> individs, IData data);
+    }
+
+    public class PenaltyFunction : IConstraintProcessing
+    {
+        public List<IndividEx> Run(List<Individ> individs, IData data)
+        {
+            Logger.Get().Debug("Called Algorithm.PenaltyFunction");
+            var individsEx = new List<IndividEx>();
+
+            int averageCost = 0;
+            for (int i = 0; i < individs.Count; i++)
+            {
+                averageCost += Helpers.GetCost(individs[i], data);
+            }
+            averageCost /= individs.Count;
+            double coeffA = 0;
+            double coeffB = 0;
+            int weight = 0;
+            int cost = 0;
+            int minCost = 0;
+
+            for (var i = 0; i < individs.Count; ++i)
+            {
+                var individEx = new IndividEx(individs[i].SIZE);
+                individEx.GENOTYPE = individs[i].GENOTYPE;
+
+                cost = Helpers.GetCost(individs[i], data);
+                weight = Helpers.GetWeight(individs[i], data);
+                individEx.WEIGHT = weight;
+                individEx.COST = weight <= data.MAX_WEIGHT ? cost : cost - (int)Math.Pow(weight - data.MAX_WEIGHT, 2);
+                individsEx.Add(individEx);
+            }
+
+            for (var i = 0; i < individs.Count; ++i)
+            {
+                if (individsEx[i].COST == minCost)
+                {
+                    individsEx[i].COST = 0;
+                }
+                else if (individsEx[i].COST <= 0)
+                {
+                    cost = individsEx[i].COST;
+                    coeffA = averageCost / (cost - averageCost);
+                    coeffB = averageCost * cost / (cost - averageCost);
+                    individsEx[i].COST = Convert.ToInt32(coeffA * cost + coeffB);
+                }
+            }
+            return individsEx;
+        }
+    }
+
+    public class RepairOperator : IConstraintProcessing
+    {
+        public List<IndividEx> Run(List<Individ> individs, IData data)
+        {
+            return null;
+        }
+    }
+}
