43db055ce996d35e1bcbf7fd8ec53758bd4a07e9
eshulankina <eljogar@mail.ru>
2018-05-05 10:41:00 +0300

    Updated correctness tests

---
+++ app/KnapcaskProblem/CorrectnessTests/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CorrectnessTests
+{
+    class Helpers
+    {
+        public static bool ExecuteWithTimeLimit(TimeSpan timeSpan, Action codeBlock)
+        {
+            try
+            {
+                Task task = Task.Factory.StartNew(() => codeBlock());
+                task.Wait(timeSpan);
+                return task.IsCompleted;
+            }
+            catch (AggregateException ae)
+            {
+                throw ae.InnerExceptions[0];
+            }
+        }
+    }
+}
--- app/KnapcaskProblem/CorrectnessTests/Program.cs
+++ app/KnapcaskProblem/CorrectnessTests/Program.cs
@@ -11,48 +11,56 @@ namespace CorrectnessTests
             Console.WriteLine("Correctness tests for KP are running...");
             var tests = Parser.Parse01KP();
 
-            //Console.WriteLine("BranchAndBound algorithm");
-            //CheckExactAlgorithm(new BranchAndBound(), tests);
-            //CheckExactAlgorithm(new BranchAndBound(new BFS()), tests);
+            Console.WriteLine("BranchAndBound algorithm");
+            CheckExactAlgorithm(new BranchAndBound(), tests);
+            CheckExactAlgorithm(new BranchAndBound(new BFS()), tests);
 
-            //Console.WriteLine("DynamicProgramming DirectApproach algorithm");
-            //CheckExactAlgorithm(new DynamicProgramming(new DirectApproach()), tests);
+            Console.WriteLine("DynamicProgramming DirectApproach algorithm");
+            CheckExactAlgorithm(new DynamicProgramming(new DirectApproach()), tests);
 
-            //Console.WriteLine("DynamicProgramming RecurrentApproach algorithm");
-            //CheckExactAlgorithm(new DynamicProgramming(new RecurrentApproach()), tests);
+            Console.WriteLine("DynamicProgramming RecurrentApproach algorithm");
+            CheckExactAlgorithm(new DynamicProgramming(new RecurrentApproach()), tests);
 
-            //Console.WriteLine("Genetic Algorithm with default parameters");
-            //CheckHeuristicAlgorithm(new GeneticAlgorithm(), tests);
+            Console.WriteLine("Genetic Algorithm with default parameters");
+            CheckHeuristicAlgorithm(new GeneticAlgorithm(), tests);
 
-            //Console.WriteLine("\nCorrectness tests for UKP are running...");
+            Console.WriteLine("\nCorrectness tests for UKP are running...");
             tests = Parser.ParseUKP();
 
             Console.WriteLine("BranchAndBound algorithm with U3 Total Upper Bound");
             CheckExactAlgorithm(new BranchAndBound(new U3Bound()), tests);
-            //CheckExactAlgorithm(new BranchAndBound(new BFS(), new U3Bound()), tests);
+            CheckExactAlgorithm(new BranchAndBound(new BFS(), new U3Bound()), tests);
 
             Console.WriteLine("GABBHybrid algorithm");
             CheckExactAlgorithm(new GABBHybrid(), tests);
 
-            //Console.WriteLine("DynamicProgramming EDUK_EX(2, 2) algorithm");
-            //CheckExactAlgorithm(new DynamicProgramming(new EDUK_EX(2, 2)), tests);
+            Console.WriteLine("DynamicProgramming EDUK_EX(2, 2) algorithm");
+            CheckExactAlgorithm(new DynamicProgramming(new EDUK_EX(2, 2)), tests);
 
-            //Console.WriteLine("DynamicProgramming ClassicalUKP algorithm");
-            //CheckExactAlgorithm(new DynamicProgramming(new ClassicalUKPApproach()), tests);
+            Console.WriteLine("DynamicProgramming ClassicalUKP algorithm");
+            CheckExactAlgorithm(new DynamicProgramming(new ClassicalUKPApproach()), tests);
 
-            //Console.WriteLine("Genetic Algorithm with default parameters");
-            //CheckHeuristicAlgorithm(new GeneticAlgorithm(), tests);
+            Console.WriteLine("Genetic Algorithm with default parameters");
+            CheckHeuristicAlgorithm(new GeneticAlgorithm(), tests);
         }
 
         static void CheckExactAlgorithm(IExactAlgorithm alg, List<ITest> tests)
         {
             foreach (var test in tests)
             {
+                long result = -1;
                 Console.WriteLine("{0}", test.Name());
-                var result = alg.Run(test.Data());
+
+                var executed = Helpers.ExecuteWithTimeLimit(TimeSpan.FromMinutes(5), () =>
+                {
+                    result = alg.Run(test.Data());
+                });
+                if (!executed)
+                {
+                    Console.WriteLine("FAIL on {0}. Stopped by timer. Execution time exceeded 5 minutes.");
+                    continue;
+                }
                 var gold = test.Gold();
-
-
                 if (result != gold)
                 {
                     Console.WriteLine("FAIL on {0}. Expected: {1}, Actual: {2}.", test.Name(), gold, result);
